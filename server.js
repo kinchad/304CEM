126b366ef1909ccae79e4c5cc88cc4bd407198b8
kinchad <kin0307003070@gmail.com>
2019-03-20 11:52:17 +0800

    base layout

--- server.js
+++ server.js
@@ -8,21 +8,37 @@ var bodyParser = require('body-parser')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended:false}))
 
+app.post('/',function(req,res){
+	var output = "test post"
+	res.send(output)
+})
+
 app.get('/',function(req,res){
-	var template = swig.compileFile(__dirname + '/public/html/cv.html')
+	var template = swig.compileFile(__dirname + '/public/html/index.html')
 	var output = template({})
 
 	res.send(output)
 })
 
 app.get('/myForm',function(req,res){
-	var template = swig.compileFile(__dirname + '/public/myForm.html')
+	var template = swig.compileFile(__dirname + '/public/html/myForm.html')
 	var output = template({})
 	res.send(output)
 })
 
-app.post('/',function(req,res){
-	var output = "test post"
+app.get('/Page1',function(req,res){
+	var template = swig.compileFile(__dirname + '/public/html/Page1.html')
+	var output = template({})
+	res.send(output)
+})
+app.get('/Page2',function(req,res){
+	var template = swig.compileFile(__dirname + '/public/html/Page2.html')
+	var output = template({})
+	res.send(output)
+})
+app.get('/Page3',function(req,res){
+	var template = swig.compileFile(__dirname + '/public/html/Page3.html')
+	var output = template({})
 	res.send(output)
 })
 
